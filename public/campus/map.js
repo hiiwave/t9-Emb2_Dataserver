52ac932360b28bb79049c827f5505734ed4ba8f6
WaveDorm <hiiwave@gmail.com>
2015-04-30 17:08:46 +0800

    ok!

--- public/campus/map.js
+++ public/campus/map.js
@@ -1,4 +1,6 @@
 $(document).ready( function() {
+  var drawer;
+
   (function initialMap(){
     var el = d3.select('#campus-map'),
       // 150 DPI image
@@ -46,30 +48,128 @@ $(document).ready( function() {
             .attr('cy', lonlat[1])
             .on("mouseover", function(){
               console.log("Mouseover deteced! " + i);
-              showSpot(i);
+              drawer.update(i);
             });
         });
     });
   })();
 
+  drawer = {
+    init: function() {
+      drawer.reqData(drawer.getIdSet(0), function(data) {
+        drawer.firstDraw(data);
+      });
+    },
+    reqData: function(idset, callback) {
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', '/reqspot');
+      xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+      xhr.onload = function() {
+        callback(drawer.preProc(JSON.parse(this.responseText)));
+      };
+      xhr.send(JSON.stringify(idset)); 
+    },
+    svgUpdator: undefined,
+    update: function(idx) {
+      drawer.reqData(drawer.getIdSet(idx), function(data) {
+        drawer.svgUpdator(data);
+      })
+    },
+    firstDraw: function(data) {
+      console.log("Drawing..");
+      var margin = {top: 30, right: 20, bottom: 30, left: 50};
+      var width = 400 - margin.left - margin.right;
+      var height = 270 - margin.top - margin.bottom;
+
+      var x = d3.time.scale()
+        .range([0, width])
+        .domain(d3.extent(data, function (d) { return d.date; }));
+      var y = d3.scale.linear()
+        .range([height, 0])
+        .domain([0, d3.max(data, function (d) { return d.noise; })]);
+      var xAxis = d3.svg.axis().scale(x).orient("bottom").ticks(5);
+      var yAxis = d3.svg.axis().scale(y).orient("left").ticks(5);
+      var dateFormat = d3.time.format('%X');
+      var valueline = d3.svg.line()
+        .x(function(d) { return x(d.date);  })
+        .y(function(d) { return y(d.noise); });
+      var svg = d3.select("#spot-data").append('svg')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .append('g').attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      svg.append('path').attr('class', 'line')
+        .attr('d', valueline(data));
+      svg.append('g').attr('class', 'x axis')
+        .attr('transform', 'translate(0, ' + height + ')')
+        .call(xAxis);
+      svg.append('g').attr('class', 'y axis')
+        .call(yAxis);
+
+      drawer.svgUpdator = function(data) {
+        x.domain(d3.extent(data, function (d) { return d.date; }));
+        y.domain([0, d3.max(data, function (d) { return d.noise; })]);
+        var svg = d3.select("#spot-data").transition();
+        svg.select('path').duration(750).attr('d', valueline(data));
+        svg.select(".x.axis").duration(750).call(xAxis);
+        svg.select(".y.axis").duration(750).call(yAxis);
+      };
+    },
+    getIdSet: function(idx) {
+      /*
+        place     time db_date_order
+        明達館       1749  1-20
+        總圖後草坪   1800  21-45
+        總圖前     1810  46-68
+        小木屋鬆餅   1820  68-84
+        校門口     1828  85-93
+        新體    1840  94-112
+        博理館     1922  113-132
+        社科院     1930  132-155
+      */
+      switch (idx) {
+        case 0:
+          return {idBegin: 1, idEnd: 20}
+          break;
+        case 1:
+          return {idBegin: 21, idEnd: 45}
+          break;
+        case 2:
+          return {idBegin: 46, idEnd: 68}
+          break;
+        case 3:
+          return {idBegin: 68, idEnd: 84}
+          break;
+        case 4:
+          return {idBegin: 85, idEnd: 93}
+          break;
+        case 5:
+          return {idBegin: 94, idEnd: 112}
+          break;
+        case 6:
+          return {idBegin: 113, idEnd: 131}
+          break; 
+        case 7:
+          return {idBegin: 133, idEnd: 155}
+          break; 
+        default:
+          console.error("No such dataset");
+      }
+    },
+    preProc: function(data) {
+      for(var key in data) {
+        data[key].date = new Date(data[key].date);
+      };
+      return data;
+    }
+  };
+  drawer.init();
+  /*
   var showSpot = function(idx) {
-    var drawer = {
+    var drawer_dep = {
       init: function() {
-        var reqbody = 
-        this.reqData();
+        var reqbody = this.reqData();
       },
       idSet: function() {
-        /*
-          place     time db_date_order
-          明達館       1749  1-20
-          總圖後草坪   1800  21-45
-          總圖前     1810  46-68
-          小木屋鬆餅   1820  68-84
-          校門口     1828  85-93
-          新體    1840  94-112
-          博理館     1922  113-132
-          社科院     1930  132-155
-        */
         switch (idx) {
           case 0:
             return {idBegin: 1, idEnd: 20}
@@ -117,9 +217,6 @@ $(document).ready( function() {
         };
         return data;
       },
-      update: function(data) {
-        
-      },
       draw: function(data) {
         console.log("Drawing..");
         var margin = {top: 30, right: 20, bottom: 30, left: 50};
@@ -155,8 +252,9 @@ $(document).ready( function() {
           .call(yAxis);
       }
     }
-    drawer.init();
+    drawer_dep.init();
   };
+  */
 });
 
   
