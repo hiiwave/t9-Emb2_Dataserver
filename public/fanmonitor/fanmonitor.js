3676d3cae343182f6efd09451a2c0be4c6ff0005
starforest <hiiwave@gmail.com>
2015-05-21 22:46:11 +0800

    .

--- public/fanmonitor/fanmonitor.js
+++ public/fanmonitor/fanmonitor.js
@@ -15,6 +15,8 @@ socket.on('newPkt', function(pkt) {
   var str = "temparature: " + pkt.temp.toFixed(2) + '<br>' + "threshold: " + pkt.threshold.toFixed(2) + '<br>';
   $('#newData').html(str);
   $('#countData').html(1 + parseInt($('#countData').html()));
+  console.log("pkt.state: " + pkt.state);
+  console.log("fanstate: " + fanstate);
   if (pkt.state && !fanstate) {
     $('#fanImg').attr('src', '/image/fan_animated.gif');
     fanstate = true;
